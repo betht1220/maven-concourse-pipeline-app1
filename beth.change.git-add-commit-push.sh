cd7aa0c2cbf3f0d393dc199f7136084adc1ec8f6
betht1220 <10212Parkview>
2019-01-31 14:06:05 -0800

    change boot 2.1.2 to 1.5.2

---
+++ beth.change.git-add-commit-push.sh
@@ -0,0 +1,6 @@
+echo "I've change pom.xml from boot starter version 1.5.2 to 2.1.2"
+echo "git status"
+echo "git add ."
+echo "git commit -m 'spring boot starter from 1.5.2 to 2.1.2'"
+echo "git push origin master.
+
