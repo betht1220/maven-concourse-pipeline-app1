c280fe03a98360bbeaa14454e0cca1bcb5d0f394
betht1220 <10212Parkview>
2019-01-31 18:37:07 -0800

    test

---
+++ beth-change.pom.repo.ip.from.127.0.0.1-to.192.168.1.78.macip.sh
@@ -0,0 +1,6 @@
+echo "I've change pom.xml ip from 127.0.0.1 to 192.168.1.78 (ifconfig | grep broadcast)"
+echo "git status"
+echo "git add ."
+echo "git commit -m 'change from 127.0.0.1 to 192.168.1.78 (ifconfig | grep broadcast)'"
+echo "git push origin master.
+
